5a94d7da3315e329ef61156cb6d42073827e7671
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-27 14:47:28 -0500

    Added a block helper to render the correct title for the dashboard page and added a route handler to access dashboard.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,6 +16,15 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/dashboard", async (req, res) => {
+  try {
+    res.render("dashboard", { isDashboardPage: true });
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
@@ -24,6 +33,7 @@ router.get("/login", (req, res) => {
 
   res.render("login");
 });
+
 router.get("/signUp", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
