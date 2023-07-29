ad27086be6586bf816dda6082b312e94d20b38d8
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-29 11:48:09 -0500

    Added a post-routes to handle posting comments and new posts. Also added id's to comment and post models.

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const { Post, Comment } = require("../../models");
+
+router.post("/", async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
